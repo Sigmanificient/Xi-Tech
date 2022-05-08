571155f8587128f837bdaf5b0e38f1297cbda5fb
sigmanificient <edhyjox@gmail.com>
2022-05-08 23:41:52 +0200

    :card_file_box: Adding some base values

---
+++ db/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (1, 'Xiaomi');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (2, 'Samsung');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (3, 'Oppo');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (4, 'Honor');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (5, 'OnePlus');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (6, 'JBL');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (7, 'Logitech');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (8, 'Steelseries');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (9, 'LG');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (10, 'Philips');
+INSERT INTO `xi-tech`.Brand (id, name) VALUES (11, 'Sony');
+
+INSERT INTO `xi-tech`.Category (id, name) VALUES (1, 'Claviers');
+INSERT INTO `xi-tech`.Category (id, name) VALUES (2, 'Souries');
+INSERT INTO `xi-tech`.Category (id, name) VALUES (3, 'Montres connectées');
+INSERT INTO `xi-tech`.Category (id, name) VALUES (4, 'Smartphones');
+INSERT INTO `xi-tech`.Category (id, name) VALUES (5, 'Enceintes');
+INSERT INTO `xi-tech`.Category (id, name) VALUES (6, 'TV');
+
+-- APP_SECRET: PoIALrBdiJzZJO8Y - Password: admin
+INSERT INTO `xi-tech`.User (id, firstname, lastname, address, postal_code, city, phone_number, email, account_type, authentication_string, salt) VALUES (2, 'Jorge', 'K. Hughley', '3693 Blue Spruce Lane', '21219', 'Sparrows Point', '4103880732', 'JorgeKHughley@rhyta.com', 4, '55fee4465c14c834bf53500b7a85e222d415a983541e876ca7500b14c52c9a5573134dd826064d86273235f18c4dfbc40afacf1274941b43e5f7164aaab20dfd', 'CNujLYf1jJJNd4Ta');
