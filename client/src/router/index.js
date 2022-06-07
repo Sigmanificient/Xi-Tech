f8208ca995f2fe3708a0e3ed2dda2a471771859f
sigmanificient <edhyjox@gmail.com>
2022-06-07 16:25:02 +0200

    :sparkles: Adding ticket route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import TicketView from "@/views/TicketView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/ticket/:id",
+      name: "ticket",
+      component: TicketView,
     },
   ],
 });
