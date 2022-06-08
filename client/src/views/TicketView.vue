4b1588128e6745c60b71ff90a8643066c9c7fc62
sigmanificient <edhyjox@gmail.com>
2022-06-08 23:25:21 +0200

    :construction: Ticket Base

---
+++ client/src/views/TicketView.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Page de Ticket</h1>
+  <div class="ticket" v-if="Object.keys(ticket).length !== 0">
+    {{ ticket }}
+  </div>
+  <div v-else>
+    <p>Chargement...</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "TicketView",
+  data() {
+    return {
+      ticket: {},
+    };
+  },
+  mounted() {
+    axios
+      .get(`/api.php?t=${this.$route.params.id}/ticket/info`)
+      .then((response) => {
+        this.ticket = response.data;
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+};
+</script>
+
+<style scoped></style>
